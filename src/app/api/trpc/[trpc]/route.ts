1e005d4c61929a2c10fc638e7f91a139c5428556
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2023-11-22 14:34:34 -0500

    Laid out the foundation of the page and layout just need to syle it

---
+++ src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,24 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { type NextRequest } from "next/server";
+
+import { env } from "util/env.mjs";
+import { appRouter } from "util/server/api/root";
+import { createTRPCContext } from "util/server/api/trpc";
+
+const handler = (req: NextRequest) =>
+  fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: () => createTRPCContext({ req }),
+    onError:
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            );
+          }
+        : undefined,
+  });
+
+export { handler as GET, handler as POST };
